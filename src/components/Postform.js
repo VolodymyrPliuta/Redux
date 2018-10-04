5769b921c58076edca10f65b944d0e289e0656f6
americanman <snowboardracer7@gmail.com>
2018-10-04 12:38:40 -0400

    Post request to REST API

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -13,11 +13,28 @@ class Postform extends Component {
     this.setState({[e.target.name]: e.target.value  })
   }
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
   render() {
     return(
       <div>
         <h1>Add Post</h1>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div>
             <label>Title: </label>
             <input type="text" name="title" onChange={this.onChange} value={this.state.value} />
