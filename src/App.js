74706e29a579143c99ce68981435ccb89f9cc74e
americanman <snowboardracer7@gmail.com>
2018-10-04 11:40:54 -0400

    Add Form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Post from './components/Post'
+import Postform from './components/Postform'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Postform />
+        <hr />
         <Post />
       </div>
     );
--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
 
 class Postform extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: '',
+      body: ''
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({[e.target.name]: e.target.value  })
+  }
+
   render() {
     return(
       <div>
-
+        <h1>Add Post</h1>
+        <form>
+          <div>
+            <label>Title: </label>
+            <input type="text" name="title" onChange={this.onChange} value={this.state.value} />
+          </div>
+          <br />
+          <div>
+            <label>Body: </label>
+            <textarea style={{borderColor: 'black'}} name="body" onChange={this.onChange} value={this.state.boby} />
+          </div>
+          <br />
+          <button type="submit">Submitt</button>
+        </form>
       </div>
     )
   }
